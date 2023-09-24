b2b7b1234859614c0e2352da61ee57979b9a5e64
Liz Trejo <liz@liztrejo.dev>
2023-09-23 17:10:35 -0700

    adds data validation to Book model

--- models/book.js
+++ models/book.js
@@ -1,9 +1,7 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Book extends Model {
+const Sequelize = require('sequelize');
+module.exports = (sequelize) => {
+  class Book extends Sequelize.Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -14,10 +12,32 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Book.init({
-    title: DataTypes.STRING,
-    author: DataTypes.STRING,
-    genre: DataTypes.STRING,
-    year: DataTypes.INTEGER
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Title is required.'
+        },
+        notNull: {
+          msg: 'Title is required.'
+        }
+      }
+    },
+    author: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Author is required.'
+        },
+        notNull: {
+          msg: 'Author is required.'
+        }
+      }
+    },
+    genre: Sequelize.STRING,
+    year: Sequelize.INTEGER
   }, {
     sequelize,
     modelName: 'Book',
