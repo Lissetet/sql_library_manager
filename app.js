3c2142e9625d03406a1c7c720f04e5be5a7b7175
Liz Trejo <liz@liztrejo.dev>
2023-09-24 18:08:16 -0700

    adds page not found and error handling views

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ sequelize.sync();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -31,20 +31,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use(express.static('public'));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  const err = new Error();
+  err.status = 404;
+  err.message = "Sorry! We couldn't find the page you were looking for.";
+  res.render('page-not-found', { err });
+  next();
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  err.message = err.message || 'Sorry! There was an unexpected error on the server.';
+  err.status = err.status || 500;
+  console.log({
+    message: err.message,
+    status: err.status,
+  })
+  res.status(err.status);
+  res.render('error', { err });
 });
 
 module.exports = app;
