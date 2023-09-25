e0828e092120da80328849c2e8f42ab5e5c6651f
Liz Trejo <liz@liztrejo.dev>
2023-09-24 18:28:20 -0700

    adds all books view at /books

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ router.get('/', (req, res)=>(res.redirect('/books')));
 /* GET book listing. */
 router.get('/books', asyncHandler(async (req, res) => {
   const books = await Book.findAll();
-  res.json(books);
+  res.render('all-books', { books });
 }));
 
 /* GET Create new book form. */
